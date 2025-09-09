ab508c3a01ac59138d388dd41376e3d67e63fac4
Sean <sle@veritone.com>
2025-09-09 18:40:37 +0700

    style: format eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,33 +1,38 @@
-import js from '@eslint/js';
-import eslintConfigPrettier from 'eslint-config-prettier';
-import tseslint from 'typescript-eslint';
+import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import tseslint from "typescript-eslint";
 
 export default [
-    js.configs.recommended,
-    ...tseslint.configs.recommended,
-    eslintConfigPrettier,
-    {
-        files: ['**/*.{ts,tsx,js,jsx}'],
-        languageOptions: {
-            parser: tseslint.parser,
-            parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-            globals: {
-                Buffer: 'readonly',
-                console: 'readonly',
-                process: 'readonly',
-                module: 'readonly',
-                setTimeout: 'readonly',
-            },
-        },
-        rules: {
-            '@typescript-eslint/no-unused-vars': 'off',      // optionally 'warn'
-            '@typescript-eslint/no-explicit-any': 'off',     // optionally 'warn'
-        },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
+  {
+    files: ["**/*.{ts,tsx,js,jsx}"],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+      globals: {
+        Buffer: "readonly",
+        console: "readonly",
+        process: "readonly",
+        module: "readonly",
+        setTimeout: "readonly",
+      },
     },
-    {
-        files: ['**/*.{test,spec}.{ts,tsx,js,jsx}'],
-        languageOptions: {
-            globals: { describe: 'readonly', it: 'readonly', expect: 'readonly', jest: 'readonly' },
-        },
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off", // optionally 'warn'
+      "@typescript-eslint/no-explicit-any": "off", // optionally 'warn'
     },
+  },
+  {
+    files: ["**/*.{test,spec}.{ts,tsx,js,jsx}"],
+    languageOptions: {
+      globals: {
+        describe: "readonly",
+        it: "readonly",
+        expect: "readonly",
+        jest: "readonly",
+      },
+    },
+  },
 ];
